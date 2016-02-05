06268516d664faa8caef849ea433fe40504cb965
Sergey <kuzmenkos1986@gmail.com>
2016-02-05 14:40:16 +0200

    firstff

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,33 +1,47 @@
 $(function(){
 
-} );
+    $('.btn-up').on({
+        'click':function(){
+            $('html, body').animate({scrollTop: 0}, 600);
+        }
 
-var Shablon = function (obj) {
-    this.obj = obj;
+    });
 
-
-    this.init();
-};
-Shablon.prototype = {
-    init: function () {
-        var self = this;
-
-        self.core = self.core();
-        self.core.build();
-    },
-    core: function () {
-        var self = this;
-
-        return {
-            addEvents: function () {
-
-            },
-            build: function () {
-                self.core.addEvents();
+    $(window).on({
+        'scroll':function(){
+            if($(window).scrollTop() > $(window).height()){
+                $('.btn-up').fadeIn();
+            } else {
+                $('.btn-up').fadeOut();
             }
-        };
-    }
-};
+            if($(window).scrollTop() + $(window).height() > $('.site__footer').offset().top + 16) {
+                $('.btn-up').css({ bottom: $(window).scrollTop() + $(window).height() - $('.site__footer').offset().bottom + 55 });
+            } else {
+                $('.btn-up').css({ bottom: '55px' });
+            }
+        }
+    });
+
+    $('.menu__btn').on({
+        'click':function(){
+            var curElem = $(this).parent();
+
+            if (curElem.hasClass('active')) {
+                curElem.removeClass('active');
+            } else {
+                curElem.addClass('active');
+            }
+
+        }
+    });
+
+    $( ".popular-last" ).click(function() {
+        $( ".site__aside__popular ul li" ).css('display', 'block');
+        $( ".popular-last").css('display', 'none');
+        return false;
+    });
+
+} );
 
 $(window).on({
     load: function () {
