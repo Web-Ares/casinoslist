24a44b681a60226ce6d472cd4dfbff40ede4049c
alex <alexnikol@e-mail.ua>
2016-02-05 20:40:40 +0200

    first

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -7,6 +7,10 @@ $(function(){
 
     });
 
+    $('.swiper-container').each(function () {
+        Slider($(this));
+    });
+
     $(window).on({
         'scroll':function(){
             if($(window).scrollTop() > $(window).height()){
@@ -42,6 +46,62 @@ $(function(){
     });
 
 } );
+
+var Slider = function (obj) {
+
+    //private properties
+    var _self = this,
+        _next = obj.find($('.swiper-button-next')),
+        _prev = obj.find($('.swiper-button-prev')),
+        _obj = obj;
+
+    //private methods
+    var _addEvents = function () {
+
+        },
+        _init = function () {
+            _addEvents();
+        };
+    if (_obj.hasClass('review-slider__wrap')) {
+        var _swiperPromo = new Swiper(_obj, {
+            nextButton: _next,
+            prevButton: _prev,
+            slidesPerView: 7,
+            spaceBetween: 0,
+            loop: false,
+            breakpoints: {
+
+                1024: {
+                    slidesPerView: 5
+                },
+                768: {
+                    slidesPerView: 3
+                }
+            }
+
+
+        });
+
+    }
+    if (_obj.hasClass('container_wrap')) {
+        var _slider = new Swiper(_obj, {
+            nextButton: '.slider_next',
+            prevButton: '.slider_prev',
+            spaceBetween: 0,
+            loop: false,
+            loopedSlides: 3,
+            slidesPerView: 6
+
+        });
+
+    }
+
+    //public properties
+
+    //public methods
+
+    _init();
+};
 
 $(window).on({
     load: function () {
